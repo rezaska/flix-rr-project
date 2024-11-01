c50e5351cb83ac6bb8c22a932d57808c08c0dafa
Reza <reza.soleimani.ka@gmail.com>
2024-11-01 16:35:38 -0400

    Updated Movie model and helper

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,7 +1,7 @@
 module MoviesHelper
 
   def total_gross(movie)
-    if movie.total_gross < 225_000_000
+    if movie.flop?
       "Flop!"
     else
       number_to_currency(movie.total_gross, precision: 0)
